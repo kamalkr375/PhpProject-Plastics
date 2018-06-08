4c896ddd5f2184a5c65d9f1fd6f5e46beeb3eb7f
kamalkr375 <37614066+kamalkr375@users.noreply.github.com>
2018-06-08 16:19:07 +0530

    Update functions.php

--- plastix/wp-content/themes/twentyfifteen/functions.php
+++ plastix/wp-content/themes/twentyfifteen/functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Twenty Fifteen functions and definitions
- *
+ *this is my first project in github
  * Set up the theme and provides some helper functions, which are used in the
  * theme as custom template tags. Others are attached to action and filter
  * hooks in WordPress to change core functionality.
